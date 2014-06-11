e98278b2efe50175f2514b56b7160df3e96068a7
Damien Raude-Morvan <damien@dictanova.com>
2014-06-11 18:01:51 +0200

    Refactor plugin to create a task for each sourceSet

--- src/main/groovy/com/dictanova/gora/gradle/GoraPlugin.groovy
+++ src/main/groovy/com/dictanova/gora/gradle/GoraPlugin.groovy
@@ -18,16 +18,50 @@ package com.dictanova.gora.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.SourceSet
 
 class GoraPlugin implements Plugin<Project> {
+    private static final String GENERATE_GROUP = 'generate'
 
     @Override
     void apply(final Project project) {
-
-        project.task(type: GoraCompileTask, 'compileGora')
-
-        project.afterEvaluate {
-            project.tasks.compileGora.execute()
+        project.apply plugin: 'java'
+        project.sourceSets.all { SourceSet sourceSet ->
+            setupGoraFor(sourceSet, project)
         }
+    }
+
+    private setupGoraFor(SourceSet sourceSet, Project project) {
+        def configName = (sourceSet.getName().equals(SourceSet.MAIN_SOURCE_SET_NAME) ? "gora" : sourceSet.getName() + "Gora")
+        project.configurations.create(configName) {
+            visible = false
+            transitive = false
+            description = "The Gora libraries to be used for this project."
+            extendsFrom = []
+        }
+
+        // Wire task
+        Task goraTask = createGoraTaskFor(sourceSet, project)
+
+        // Generate source code before java compile
+        String compileJavaTaskName = sourceSet.getCompileTaskName("java");
+        Task compileJavaTask = project.tasks.getByName(compileJavaTaskName);
+        compileJavaTask.dependsOn(goraTask)
+    }
+
+    private Task createGoraTaskFor(SourceSet sourceSet, Project project) {
+        def taskName = taskName(sourceSet)
+        def goraTask = project.tasks.create(taskName, GoraCompileTask)
+        goraTask.group = GENERATE_GROUP
+        goraTask.description = "Generates code from the ${sourceSet.name} Gora schemas."
+        goraTask.source = 'src/main/resources/'
+        goraTask.destinationDir = project.file('src/main/java')
+
+        goraTask
+    }
+
+    private String taskName(SourceSet sourceSet) {
+        return sourceSet.getTaskName('generate', 'Gora')
     }
 }
